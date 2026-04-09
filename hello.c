127a535b5426bb825cddcbf6ec546f995044b237
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-04-10 00:25:58 +0530

    initial

---
+++ hello.c
@@ -0,0 +1,67 @@
+// #include <stdio.h> 
+
+// int main() {        
+//     printf("Hello, World!\n"); 
+//     return 0;       
+// }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int a = 10, b = 20;
+//     int sum = a + b;
+//     printf("Sum of %d and %d is %d\n", a, b, sum);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int age;
+//     printf("Enter your age: ");
+//     scanf("%d", &age);  // Reads an integer and stores it in 'age'
+//     printf("You are %d years old.\n", age);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 1; i <= 5; i++) {
+//         printf("Iteration %d\n", i);
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int num;
+//     printf("Enter a number: ");
+//     scanf("%d", &num);
+
+//     if (num > 0) {
+//         printf("The number is positive.\n");
+//     } else {
+//         printf("The number is negative or zero.\n");
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int age;
+//     printf("Enter your age: ");
+//     scanf("%d", &age);  // Reads an integer and stores it in 'age'
+//     printf("You are %d years old.\n", age);
+//     return 0;
+// }
+
+
+
