890ed455c951ac9753cfd894d94262c54f42e18c
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-04-18 00:18:23 +0530

    Add files via upload

---
+++ lec4.c
@@ -0,0 +1,112 @@
+// #include<stdio.h>
+// float squar(float side );
+// float circle(float r);
+// float rectangle(float l,float b);
+// int main(){
+//   float x;
+//   printf("enter the side of the square   ");
+//   scanf("%f",&x);
+//   float s= squar(x);
+//   printf("area of the square is    %f\n",s);
+//   float r;
+//   printf("enter the radius of circle   ");
+//   scanf("%f",&r);
+//   float k = circle(r);
+//   printf("area of the circle is %f\n",k);
+// #include<stdio.h>
+// float squar(float side );
+// float circle(float r);
+// float rectangle(float l,float b);
+// int main(){
+//   float x;
+//   printf("enter the side of the square   ");
+//   scanf("%f",&x);
+//   float s= squar(x);
+//   printf("area of the square is    %f\n",s);
+//   float r;
+//   printf("enter the radius of circle   ");
+//   scanf("%f",&r);
+//   float k = circle(r);
+//   printf("area of the circle is %f\n",k);
+
+//   float l,b;
+//   printf("enter the value of l and b ");
+//   scanf("%f%f",&l,&b);
+//   float p=rectangle(l,b);
+//   printf("the area of rectangle is %f\n",p);
+//   float l,b;
+//   printf("enter the value of l and b ");
+//   scanf("%f%f",&l,&b);
+//   float p=rectangle(l,b);
+//   printf("the area of rectangle is %f\n",p);
+
+//   return 0;
+// }
+// float squar(float side){
+//  return side*side;
+// }
+// float circle(float r){
+//   return r*3.14*r;
+// }
+// float rectangle(float l,float b){
+//   return l*b;
+// }
+//   return 0;
+// }
+// float squar(float side){
+//  return side*side;
+// }
+// float circle(float r){
+//   return r*3.14*r;
+// }
+// float rectangle(float l,float b){
+//   return l*b;
+// }
+
+
+// #include<stdio.h>
+// @@ -85,4 +85,23 @@ float rectangle(float l,float b){
+// }
+// int sum(int a,int b){
+//   return a+b;
+// }
+// }
+
+// #include<stdio.h>
+
+
+// int main(){
+//   int a;
+//   printf("enter the Nth number  ");
+//   scanf("%d",&a);
+//   int sum = 0;
+//   for(int i=1;i<=a;i++){
+    
+    
+//     sum=sum+i;
+  
+//   }
+//   printf("%d",sum);
+// return 0;
+
+// }
+
+
+#include<stdio.h>
+
+
+int main(){
+  int a;
+  printf("enter the Nth number  ");
+  scanf("%d",&a);
+  int sum = 0;
+  for(int i=1;i<=a;i++){
+    
+    
+    sum=sum+i;
+  
+  }
+  printf("%d",sum);
+return 0;
+
+}
