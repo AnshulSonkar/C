9c56ced80b4349ee32c4bff002d7bfc035267f04
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-05-02 23:59:00 +0530

    Add files via upload

--- lec7.c
+++ lec7.c
@@ -43,31 +43,61 @@
 // } 
 
 
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b;
+//     char ch;
+//     printf("Enter Two Numbers : ");
+//     scanf("%d%d",&a,&b);
+//     printf("\nExamaple : +,-,*,/");
+//     printf("Enter Choice : ");
+//     scanf(" %c",&ch);
+//     switch(ch)
+//     {
+//         case '+' : printf("\nThe addition of two number : %d",a+b);
+//                     break;
+//         case '-' : printf("\nThe addition of two number : %d",a-b);
+//                     break;
+//         case '*' : printf("\nThe addition of two number : %d",a*b);
+//                     break;
+//         case '/' : if(b!=0)
+//                         printf("\nThe addition of two number : %.2f",(float)a/b);
+//                     else 
+//                         printf("\nCannot devide by zero");
+//                     break;
+
+//         default : printf("\nInvalid Input");
+
+//     }
+// } 
+
+
 #include<stdio.h>
 int main()
 {
-    int a,b;
-    char ch;
-    printf("Enter Two Numbers : ");
-    scanf("%d%d",&a,&b);
-    printf("\nExamaple : +,-,*,/");
-    printf("Enter Choice : ");
-    scanf(" %c",&ch);
+    int rup,ch;
+    printf("Enter rupee : ");
+    scanf("%d",&rup);
+    printf("\nChoices : ");
+    printf("\n Enter 1 : Doller");
+    printf("\n Enter 2 : Euro");
+    printf("\n Enter 3 : Canadian Doller");
+    printf("\n Enter 4 : Saudi Riyal");
+    printf("\nEnter choice : ");
+    scanf("%d",&ch);
+
     switch(ch)
     {
-        case '+' : printf("\nThe addition of two number : %d",a+b);
+        case 1 : printf("\nIn doller : %.2f",(float)rup/87.85);
                     break;
-        case '-' : printf("\nThe addition of two number : %d",a-b);
+        case 2 : printf("\nIn Euro : %.2f",(float)rup/102.39);
                     break;
-        case '*' : printf("\nThe addition of two number : %d",a*b);
+        case 3 : printf("\nIn Canadian Doller : %.2f",(float)rup/63.71);
                     break;
-        case '/' : if(b!=0)
-                        printf("\nThe addition of two number : %.2f",(float)a/b);
-                    else 
-                        printf("\nCannot devide by zero");
+        case 4 : printf("\nIn Saudi Riyal : %.2f",(float)rup/23.85);
                     break;
-
         default : printf("\nInvalid Input");
-
+        
     }
 }
