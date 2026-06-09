e0aa5a436782834dd87eb7eeccbbfb5ec9ef8717
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-06-09 10:57:16 +0530

    Add files via upload

--- Q1.c
+++ Q1.c
@@ -1,18 +1,10 @@
-// Swapping of two nodes using bitwise operators
-
 #include<stdio.h>
+int sum()
+{
+    int a=5,b=10;
+    printf("%d",a+b);
+}
 int main()
 {
-    int a = 5;
-    int b = 3;
-
-    printf("Before a = %d b = %d\n",a,b);
-
-
-    a = a^b;
-    b = a^b;
-    a = a^b;
-
-    printf("After a = %d b = %d",a,b);
-
+    sum();
 }
