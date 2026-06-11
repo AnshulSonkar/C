d1cc7dbf8d76c049db628f88133bb2ff2479110d
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-06-12 00:16:47 +0530

    Add files via upload

--- Q1.c
+++ Q1.c
@@ -1,10 +1,29 @@
 #include<stdio.h>
-int sum()
-{
-    int a=5,b=10;
-    printf("%d",a+b);
-}
 int main()
 {
-    sum();
+    int a=1234;
+    int n=a,last,sum;
+    for(;n!=0;)
+    {            
+            sum=0;
+            for(;n!=0;n=n/10)
+            {
+                int last=n%10;
+                sum=sum+last;
+               // printf("%d\n",sum);
+                    
+            }
+            if(sum>9)
+            {
+                n=sum;
+            }
+    }
+    if(sum==1)
+    {
+        printf("It's a magic number !!");
+    }
+    else
+    {
+        printf("It's not a magic number !! %d",sum);
+    }
 }
