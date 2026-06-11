46d880875e7d09d6bc523e15a3a2f238d98edf69
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-06-12 00:19:34 +0530

    Add files via upload

--- Q5.c
+++ Q5.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-int sum()
-{
-    int a=5,b=10;
-    int c=a+b;
-    return c;
-}
 int main()
 {
-    int data=sum();
-    printf("%d",data);
-
+    int arr[5]={1,6,0,4,2};
+    int sum=0;
+    for(int i=0;i<5;i++)
+    {
+        sum=sum+arr[i];
+        
+    }
+    printf("Sum of array : %d",sum);
 }
