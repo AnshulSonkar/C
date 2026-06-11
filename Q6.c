51b8392a9253e705539c08853958f44fe8be48ed
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-06-12 00:20:06 +0530

    Add files via upload

--- Q6.c
+++ Q6.c
@@ -1,35 +1,18 @@
-//WAP to find magic number if sum digit =1 (magic)
-//1+2+3+4=10=1+0=1(Magic)
-
 #include<stdio.h>
-int magic(int n)
-{
-    int sum=0;
-    int b=n;
-    l1:
-    for(sum=0;b!=0;b=b/10)
-    {
-        int last=b%10;
-        sum=sum+last;
-    }
-    if(sum>=10)
-    {
-        b=sum;
-        
-        goto l1;
-    }
-    else
-    {
-        if(sum==1)
-            printf("magic");
-        else
-            printf("not magic");
-    }
-}
 int main()
 {
-    int n;
-    printf("Enter a number : ");
-    scanf("%d",&n);
-    magic(n);
+    int a[5]={1,0,5,9,7};
+    int max=a[0],min=a[0];
+    for(int i=0;i<5;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+        if(min>a[i])
+        {
+            min=a[i];
+        }
+    }
+    printf("Min = %d max = %d",min,max);
 }
