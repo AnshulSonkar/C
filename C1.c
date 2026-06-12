a7553ecb0213f40ee6904b816bb6b95716220a90
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-06-12 11:36:07 +0530

    Add files via upload

--- C1.c
+++ C1.c
@@ -1,22 +1,61 @@
-// Concat of array
+//Binary Search : 
 
 #include<stdio.h>
 int main()
 {
-    int a[3]={1,2,3};
-    int b[3]={4,5,6};
-    int c[6],i,j;
-    for(i=0;i<3;i++)
+    int a[5]={1,2,5,9,11};
+    int n;
+    int size=sizeof(a)/sizeof(a[0]);
+    int l=0,h=size-1;
+    int mid;
+    int i,j;
+    for(i=0;i<size;i++)
     {
-        c[i]=a[i];
+        for(j=i+1;j<size;j++)
+        {
+            
+            if(a[i]>a[j])
+            {
+                int t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+            
+        }
     }
-    for(j=0;j<3;j++,i++)
+    for(i=0;i<size;i++)
     {
-        c[i]=b[j];
+        printf("%d\t",a[i]);
     }
-    for (i = 0; i < 6; i++)
+    printf("\n");
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    for(;l<=h;)
     {
-        printf("%d\t",c[i]);
+        mid=(l+h)/2;
+        if(a[mid]==n)
+        {
+            break;
+        }
+        else
+        {
+            if(n>mid)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                h=mid-1;
+            }
+         
+        }
     }
-    
+    if(h<l)
+    {
+        printf("Element Not Found");
+    }
+    else
+    {
+        printf("Element Found");
+    }
 }
