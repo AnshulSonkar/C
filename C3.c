d832c7e57d1a1b12b7b1ce6e0b43029274e1bf7e
ANSHUL SONKAR <anshulsonkar2222@gmail.com>
2026-06-12 12:02:17 +0530

    Add files via upload

--- C3.c
+++ C3.c
@@ -1,15 +1,33 @@
-#include<stdio.h>
-int main()
-{
-    int a[5]={10,20,30,40,50},i;
-    int t=a[0];
-    for(i=0;i<4;i++)
-    {
-        a[i]=a[i+1];
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+
+        if (key < arr[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
     }
-    a[i]=t;
-    for(i=0;i<5;i++)
-    {
-        printf("\t%d",a[i]);
-    }
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {5, 10, 15, 20, 25};
+    int n = 5;
+
+    int pos = binarySearch(arr, n, 20);
+
+    if (pos != -1)
+        printf("Found at index %d", pos);
+    else
+        printf("Not Found");
+
+    return 0;
 }
