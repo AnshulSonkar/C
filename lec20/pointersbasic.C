ad5228d30075358fb0e590ac92b96143893bb91a
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 23:58:27 +0530

    Pointers

--- lec20/pointersbasic.C
+++ lec20/pointersbasic.C
@@ -1,13 +1,27 @@
+// #include <stdio.h>
+
+// int main() {
+
+//     int x = 10;
+//     int *ptr = &x;
+
+//     printf("%d\n", x);
+//     printf("%p\n", &x);
+//     printf("%p\n", ptr);
+
+//     return 0;
+// } 
+
+
+// Differencing Pointers
 #include <stdio.h>
 
 int main() {
 
-    int x = 10;
+    int x = 50;
     int *ptr = &x;
 
-    printf("%d\n", x);
-    printf("%p\n", &x);
-    printf("%p\n", ptr);
+    printf("%d\n", *ptr);
 
     return 0;
 }
