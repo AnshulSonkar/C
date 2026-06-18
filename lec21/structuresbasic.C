993a4519df5aed4b4c9c071d7430f8c47fdd7697
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-18 23:51:13 +0530

    Structures

--- lec21/structuresbasic.C
+++ lec21/structuresbasic.C
@@ -1,8 +1,31 @@
+// Structres
+// #include <stdio.h>
+
+// struct Student {
+//     int roll;
+//     float cgpa;
+// };
+
+// int main() {
+
+//     struct Student s1;
+
+//     s1.roll = 101;
+//     s1.cgpa = 8.5;
+
+//     printf("Roll = %d\n", s1.roll);
+//     printf("CGPA = %.2f\n", s1.cgpa);
+
+//     return 0;
+// } 
+
+
+// Structre with string
 #include <stdio.h>
 
 struct Student {
     int roll;
-    float cgpa;
+    char name[50];
 };
 
 int main() {
@@ -10,10 +33,12 @@ int main() {
     struct Student s1;
 
     s1.roll = 101;
-    s1.cgpa = 8.5;
+
+    printf("Enter Name: ");
+    scanf("%s", s1.name);
 
     printf("Roll = %d\n", s1.roll);
-    printf("CGPA = %.2f\n", s1.cgpa);
+    printf("Name = %s\n", s1.name);
 
     return 0;
 }
