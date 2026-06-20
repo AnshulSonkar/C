8dcf71f099a0b1ad0eedd4d294ca29a2bbb57cf9
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-21 00:05:23 +0530

    Recursion

--- Recursion/Recursion_intro.C
+++ Recursion/Recursion_intro.C
@@ -99,24 +99,43 @@
 
 
 // Fibonacci Series 
+// #include <stdio.h>
+
+// int fib(int n) {
+
+//     if(n == 0)
+//         return 0;
+
+//     if(n == 1)
+//         return 1;
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// int main() {
+
+//     for(int i = 0; i < 8; i++) {
+//         printf("%d ", fib(i));
+//     }
+
+//     return 0;
+// } 
+
+
+// Power of Number 
 #include <stdio.h>
 
-int fib(int n) {
+int power(int a, int b) {
 
-    if(n == 0)
-        return 0;
-
-    if(n == 1)
+    if(b == 0)
         return 1;
 
-    return fib(n - 1) + fib(n - 2);
+    return a * power(a, b - 1);
 }
 
 int main() {
 
-    for(int i = 0; i < 8; i++) {
-        printf("%d ", fib(i));
-    }
+    printf("%d", power(2, 5));
 
     return 0;
 }
