1f1fa1b50ac45a46a6975dbdcc1e66e3bb170302
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-23 21:37:57 +0530

    Preprocessor Directives

--- lec26/#ifdef.C
+++ lec26/#ifdef.C
@@ -39,4 +39,18 @@
 // #endif
 
 //     return 0;
+// } 
+
+
+// Predefined Macros
+// #include <stdio.h>
+
+// int main() {
+
+//     printf("File : %s\n", __FILE__);
+//     printf("Line : %d\n", __LINE__);
+//     printf("Date : %s\n", __DATE__);
+//     printf("Time : %s\n", __TIME__);
+
+//     return 0;
 // }
