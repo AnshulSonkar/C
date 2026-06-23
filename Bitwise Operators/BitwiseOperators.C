e7a0985a7091ac6987b6e5e19c9eda7c40548e06
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-23 21:02:54 +0530

    Bitwise Operators

--- Bitwise Operators/BitwiseOperators.C
+++ Bitwise Operators/BitwiseOperators.C
@@ -1,4 +1,18 @@
 // Bitwise AND
+// #include <stdio.h>
+
+// int main() {
+
+//     int a = 5;
+//     int b = 3;
+
+//     printf("%d", a & b);
+
+//     return 0;
+// } 
+
+
+// Bitwise OR
 #include <stdio.h>
 
 int main() {
@@ -6,7 +20,7 @@ int main() {
     int a = 5;
     int b = 3;
 
-    printf("%d", a & b);
+    printf("%d", a | b);
 
     return 0;
 }
