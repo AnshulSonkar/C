bfbb3a5506627659eb652789ebf81143305e1988
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-24 14:35:00 +0530

    Command Line Arguments

--- Arguments/CommandLine.C
+++ Arguments/CommandLine.C
@@ -114,4 +114,18 @@
 //         printf("%d", b);
 
 //     return 0;
+// } 
+
+
+// Reverse Arguments
+// #include <stdio.h>
+
+// int main(int argc, char *argv[])
+// {
+//     for(int i = argc - 1; i >= 0; i--)
+//     {
+//         printf("%s\n", argv[i]);
+//     }
+
+//     return 0;
 // }
